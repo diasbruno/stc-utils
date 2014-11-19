45bf79e4b2f87a3aa2a705565fc89d7b8b13fc52
Bruno Dias <dias.h.bruno@gmail.com>
2014-11-19 19:40:13 +0000

    added documentation.

--- src/cli.php
+++ src/cli.php
@@ -1,5 +1,10 @@
 <?php
 
+/**
+ * Print to the console with a new line.
+ * @param $str string | The string.
+ * @return void
+ */
 function printLn($str = '')
 {
   print_r($str. PHP_EOL);
--- src/date.php
+++ src/date.php
@@ -1,16 +1,28 @@
 <?php
 
+/**
+ * Find a date from a filename string.
+ * ex. '2010-01-01-rest_of_the_file.ext' results in ["2010","01","01"].
+ * @param $filename string | The filename string.
+ * @return array
+ */
 function date_from_filename($filename = '')
 {
   $pattern = '/(\d{4}-\d{2}-\d{2}).+$/';
 
   if (preg_match($pattern, $filename, $match)) {
-    return join('', explode('-', $match[1]));
+    return explode('-', $match[1]);
   }
 
   return '';
 }
 
+/**
+ * Find a date from a filename string and return as YYYYMMDD.
+ * ex. '2010-01-01-rest_of_the_file.ext' results in "20100101".
+ * @param $filename string | The filename string.
+ * @return string
+ */
 function format_date_YYYYMMDD($filename = '')
 {
   $dff = date_from_filename($filename);
