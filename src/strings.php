d867e12cd9ddbf083b5ff96a1d430418f27238e2
Bruno Dias <dias.h.bruno@gmail.com>
2014-11-19 19:26:22 +0000

    deprecated to_slug in favor of cocur/slugify.

--- src/strings.php
+++ src/strings.php
@@ -1,13 +1,2 @@
 <?php
 
-function to_slug($string) {
-  // lower case
-  $string = strtolower($string);
-  // make alphanumeric (removes all other characters)
-  $string = preg_replace("/[^a-z0-9_\s-]/", "", $string);
-  // clean up multiple dashes or whitespaces
-  $string = preg_replace("/[\s-]+/", " ", $string);
-  // convert whitespaces and underscore to dash
-  $string = preg_replace("/[\s_]/", "-", $string);
-  return $string;
-}
