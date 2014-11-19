8badbf2e6ece41360c8603a442ed75213fcdf6b1
Bruno Dias <and.coffee.code@gmail.com>
2014-11-19 03:51:20 +0000

    deprecated dependency php-templates.

--- src/templates.php
+++ src/templates.php
@@ -1,30 +1,17 @@
 <?php
 
 /**
- * Takes a template and set the variables it needs.
- * @param Template $from_template Template object with the path to fetch from.
- * @return mixed   $options       List of variables.
- * view :: Template -> [] -> Template
+ * Render a view with .
+ * @param $template string | The template filename.
+ * @param $scope array | The hash with the components to be injected.
+ * @return string
  */
-function set_template_options($template, $options = [])
+function view($page, $scope = [])
 {
-    foreach($options as $var => $content) {
-        $template->set($var, $content);
-    }
-
-    return $template;
-}
-
-/**
- * Creates the page header.
- * @param Template $template The template with the right folder.
- * @param array $options     The hash with the components to be injected.
- * @return Template
- * header_page :: Template -> Map String a -> Template
- */
-function view($page, $options = [])
-{
-    $template = new Template(templates_path());
-    $template = set_template_options($template, $options);
-    return $template->fetch($page);
+	extract($scope);
+	ob_start();
+	include $page;
+	$contents = ob_get_contents();
+	ob_end_clean();
+	return $contents;
 }
